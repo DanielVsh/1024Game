d83002decc97a788e9f938c0d306b13b83c3c6ef
dv121ns <daniel.vishnievskyi@student.tuke.sk>
2022-10-01 11:49:22 +0200

    !

--- src/main/java/daniel/vishnievskyi/GameServer.java
+++ src/main/java/daniel/vishnievskyi/GameServer.java
@@ -1,14 +1,12 @@
-package daniel.vishnievskyi.server;
+package daniel.vishnievskyi;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
-@EntityScan(basePackages = "sk.tuke.kpi.kp.game.entity")
 public class GameServer {
   public static void main(String[] args) {
     SpringApplication.run(GameServer.class);
